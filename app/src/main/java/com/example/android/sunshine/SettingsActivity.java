6af3f0b693f5547f7f428651dd172322ffe9b489
James Le <le_j6@denison.edu>
2017-11-02 09:42:39 +0700

    Create the PreferenceFragment in Sunshine

--- app/src/main/java/com/example/android/sunshine/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -5,7 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 /**
- * Loads the SettingsFragment and handles the proper behavior of the up button.
+ * SettingsActivity is responsible for displaying the {@link SettingsFragment}. It is also
+ * responsible for orchestrating proper navigation when the up button is clicked. When the up
+ * button is clicked from the SettingsActivity, we want to navigate to the Activity that the user
+ * came from to get to the SettingsActivity.
+ * <p>
+ * For example, when the user is in the DetailActivity and clicks the settings option in the menu,
+ * and then clicks the up button, we want to navigate back to the DetailActivity. If the user
+ * navigates to the SettingsActivity from the MainActivity, then we want to navigate back to the
+ * MainActivity when the user clicks the up button from the SettingsActivity.
  */
 public class SettingsActivity extends AppCompatActivity {
 
