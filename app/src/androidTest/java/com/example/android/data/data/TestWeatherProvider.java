707b20f40df15f3a461d1d92cc354bd4b4a1d0ce
James Le <le_j6@denison.edu>
2017-11-03 13:23:36 +0700

    bulkInsert

--- app/src/androidTest/java/com/example/android/data/data/TestWeatherProvider.java
+++ app/src/androidTest/java/com/example/android/data/data/TestWeatherProvider.java
@@ -1,6 +1,7 @@
 package com.example.android.data.data;
 
 import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -13,11 +14,14 @@ import android.support.test.runner.AndroidJUnit4;
 
 import com.example.android.sunshine.data.WeatherContract;
 import com.example.android.sunshine.data.WeatherDbHelper;
+import com.example.android.sunshine.data.WeatherProvider;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static com.example.android.data.data.TestUtilities.BULK_INSERT_RECORDS_TO_INSERT;
+import static com.example.android.data.data.TestUtilities.createBulkInsertTestWeatherValues;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 import static junit.framework.Assert.fail;
@@ -189,6 +193,125 @@ public class TestWeatherProvider {
         com.example.android.data.data.TestUtilities.validateThenCloseCursor("testBasicWeatherQuery",
                 weatherCursor,
                 testWeatherValues);
+    }
+
+    /**
+     * This test test the bulkInsert feature of the ContentProvider. It also verifies that
+     * registered ContentObservers receive onChange callbacks when data is inserted.
+     * <p>
+     * It finally queries the ContentProvider to make sure that the table has been successfully
+     * inserted.
+     * <p>
+     * Potential causes for failure:
+     * <p>
+     *   1) Within {@link WeatherProvider#delete(Uri, String, String[])}, you didn't call
+     *    getContext().getContentResolver().notifyChange(uri, null) after performing an insertion.
+     * <p>
+     *   2) The number of records the ContentProvider reported that it inserted do no match the
+     *    number of records we inserted into the ContentProvider.
+     * <p>
+     *   3) The size of the Cursor returned from the query does not match the number of records
+     *    that we inserted into the ContentProvider.
+     * <p>
+     *   4) The data contained in the Cursor from our query does not match the data we inserted
+     *    into the ContentProvider.
+     * </p>
+     */
+    @Test
+    public void testBulkInsert() {
+
+        /* Create a new array of ContentValues for weather */
+        ContentValues[] bulkInsertTestContentValues = createBulkInsertTestWeatherValues();
+
+        /*
+         * TestContentObserver allows us to test weather or not notifyChange was called
+         * appropriately. We will use that here to make sure that notifyChange is called when a
+         * deletion occurs.
+         */
+        TestUtilities.TestContentObserver weatherObserver = TestUtilities.getTestContentObserver();
+
+        /*
+         * A ContentResolver provides us access to the content model. We can use it to perform
+         * deletions and queries at our CONTENT_URI
+         */
+        ContentResolver contentResolver = mContext.getContentResolver();
+
+        /* Register a content observer to be notified of changes to data at a given URI (weather) */
+        contentResolver.registerContentObserver(
+                /* URI that we would like to observe changes to */
+                WeatherContract.WeatherEntry.CONTENT_URI,
+                /* Whether or not to notify us if descendants of this URI change */
+                true,
+                /* The observer to register (that will receive notifyChange callbacks) */
+                weatherObserver);
+
+        /* bulkInsert will return the number of records that were inserted. */
+        int insertCount = contentResolver.bulkInsert(
+                /* URI at which to insert data */
+                WeatherContract.WeatherEntry.CONTENT_URI,
+                /* Array of values to insert into given URI */
+                bulkInsertTestContentValues);
+
+        /*
+         * If this fails, it's likely you didn't call notifyChange in your insert method from
+         * your ContentProvider.
+         */
+        weatherObserver.waitForNotificationOrFail();
+
+        /*
+         * waitForNotificationOrFail is synchronous, so after that call, we are done observing
+         * changes to content and should therefore unregister this observer.
+         */
+        contentResolver.unregisterContentObserver(weatherObserver);
+
+        /*
+         * We expect that the number of test content values that we specify in our TestUtility
+         * class were inserted here. We compare that value to the value that the ContentProvider
+         * reported that it inserted. These numbers should match.
+         */
+        String expectedAndActualInsertedRecordCountDoNotMatch =
+                "Number of expected records inserted does not match actual inserted record count";
+        assertEquals(expectedAndActualInsertedRecordCountDoNotMatch,
+                insertCount,
+                BULK_INSERT_RECORDS_TO_INSERT);
+
+        /*
+         * Perform our ContentProvider query. We expect the cursor that is returned will contain
+         * the exact same data that is in testWeatherValues and we will validate that in the next
+         * step.
+         */
+        Cursor cursor = mContext.getContentResolver().query(
+                WeatherContract.WeatherEntry.CONTENT_URI,
+                /* Columns; leaving this null returns every column in the table */
+                null,
+                /* Optional specification for columns in the "where" clause above */
+                null,
+                /* Values for "where" clause */
+                null,
+                /* Sort by date from smaller to larger (past to future) */
+                WeatherContract.WeatherEntry.COLUMN_DATE + " ASC");
+
+        /*
+         * Although we already tested the number of records that the ContentProvider reported
+         * inserting, we are now testing the number of records that the ContentProvider actually
+         * returned from the query above.
+         */
+        assertEquals(cursor.getCount(), BULK_INSERT_RECORDS_TO_INSERT);
+
+        /*
+         * We now loop through and validate each record in the Cursor with the expected values from
+         * bulkInsertTestContentValues.
+         */
+        cursor.moveToFirst();
+        for (int i = 0; i < BULK_INSERT_RECORDS_TO_INSERT; i++, cursor.moveToNext()) {
+            TestUtilities.validateCurrentRecord(
+                    "testBulkInsert. Error validating WeatherEntry " + i,
+                    cursor,
+                    bulkInsertTestContentValues[i]);
+        }
+
+        /* Always close the Cursor! */
+        cursor.close();
     }
 
     /**
